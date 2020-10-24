b9d27ab9dd10376e46ee2ec42ecf1ea0a92a5c87
jiniyash <sajanimayadunne@gmail.com>
2020-10-24 06:19:27 -0700

    Backend of finance management

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,14 @@ from rest_framework import serializers
 from .models import reservation
 from .models import room
 from .models import room_offer
-from .models import customer
+from .models import (
+customer,
+report,
+confirmation,
+outstanding,
+
+)
+
 
 class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,3 +30,23 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = customer
         fields = '__all__'
+
+
+
+#jiniseroializers
+
+class reportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = report
+        fields = ['id','date','time','details','trans_id','amount']
+
+class confirmationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = confirmation
+        fields = ['id','con_date','con_time','con_details','con_trans_id','con_amount','con_status']
+
+class outstandingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = outstanding
+        fields = ['id','out_date','out_time','out_details','out_trans_id','out_amount','out_status']
+
--- api/urls.py
+++ api/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
-from . import views
 
-urlpatterns  = [
-    path('api/', views.get_api_url_patterns),
-    path('res/', views.Reservation,name="reservation"),
-    path('reservation/<str:pk>/', views.GetOneReservation, name="getoneresevation"),
-    path('reservation-create/', views.ReservationCreate, name="rescreate"),
-    path('reservation-delete/<str:pk>/', views.ResDelete, name="resdelete"),
-    path('reservation-edit/<str:pk>/', views.ResUpdate, name="resupdate"),
-    
-    path('get-rooms/', views.GetRooms,name="room"),
+from api.views import reportViewSet,confirmationViewSet,outstandingViewSet,outstandingViewSetStatus,confirmViewSetStatus
 
-    path('get-customer/', views.GetCustomer,name="customer"),
-    path('customer-create/', views.CustomerCreate, name="custcreate"),
-    path('customer-delete/<str:pk>/', views.CustDelete, name="custdelete"),
+from rest_framework.routers import DefaultRouter
 
-]
+router = DefaultRouter()
+
+
+#sajini
+router.register(r'reportR',reportViewSet, basename='reportR' )
+router.register(r'confirmation', confirmationViewSet, basename='confirmation')
+router.register(r'outstanding', outstandingViewSet, basename='outstanding')
+router.register(r'outstandingStatus', outstandingViewSetStatus, basename="outstat")
+router.register(r'confirmStatus',confirmViewSetStatus,basename="constat")
+#sajini
+
+
+urlpatterns = router.urls
